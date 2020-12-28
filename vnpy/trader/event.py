b01a93a0d0346c7a4a2dd08f47963b2a5ab699d0
jennyliu19960126 <76522318+jennyliu19960126@users.noreply.github.com>
2020-12-28 14:21:30 +0800

    event type罗列

--- vnpy/trader/event.py
+++ vnpy/trader/event.py
@@ -2,8 +2,10 @@
 Event type string used in VN Trader.
 """
 
-from vnpy.event import EVENT_TIMER  # noqa
+# 定时器事件
+EVENT_TIMER = "eTimer"
 
+# main_engine通用事件
 EVENT_TICK = "eTick."
 EVENT_TRADE = "eTrade."
 EVENT_ORDER = "eOrder."
@@ -11,3 +13,25 @@ EVENT_POSITION = "ePosition."
 EVENT_ACCOUNT = "eAccount."
 EVENT_CONTRACT = "eContract."
 EVENT_LOG = "eLog"
+
+# Cta相关事件
+EVENT_CTA_LOG = "eCtaLog"
+EVENT_CTA_STRATEGY = "eCtaStrategy"
+EVENT_CTA_STOPORDER = "eCtaStopOrder"
+
+# 数据记录相关事件
+EVENT_RECORDER_LOG = "eRecorderLog"
+EVENT_RECORDER_UPDATE = "eRecorderUpdate"
+
+# 数据库相关事件
+EVENT_DB_LOG = "eDatabaseLog"
+
+# 算法交易相关事件
+EVENT_ALGO_LOG = "eAlgoLog"
+EVENT_ALGO_SETTING = "eAlgoSetting"
+EVENT_ALGO_VARIABLES = "eAlgoVariables"
+EVENT_ALGO_PARAMETERS = "eAlgoParameters"
+
+# 股票交易相关事件
+EVENT_EQUITY_LOG = "eEquityLog"
+EVENT_EQUITY_STRATEGY = "eEquityStrategy"
